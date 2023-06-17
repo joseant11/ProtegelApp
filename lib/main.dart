9402c76784085fa8843af517e4018149464e5b0b
Alexis Almonte <Alexisfco10@gmail.com>
2023-06-17 12:28:00 -0400

    creando enlace entre la app y firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,22 @@
 import 'package:protegelapp/screens/welcome_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home:WelcomeScreen(),
+      home: WelcomeScreen(),
     );
   }
 }
-
-
-
-
